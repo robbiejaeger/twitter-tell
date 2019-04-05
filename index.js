12c6ded0e88cf59345f9f56fdb9724e026757ac7
Robbie Jaeger <jaeger.rob@gmail.com>
2019-04-04 21:26:57 -0600

    Setup sendgrid email

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
 
-console.log(process.env.SENDGRID_API_KEY);
+function sendEmail() {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msg = {
+    to: 'jaeger.rob@gmail.com',
+    from: 'lumphammer@twittertell.com',
+    subject: 'New Lump Hammers Available',
+    html: `<h1>Lump Hammer Alert</h1>
+           <p>See if there are some new lump hammers available by Crucible Tools</p>
+           <a href="https://twitter.com/RudeMechanic" target="_blank">Chris Schwarz Twitter</a>
+           <a href="https://crucibletool.com/products/crucible-lump-hammer" target="_blank">Crucible Tool Lump Hammer</a>`
+  };
+
+  sgMail.send(msg);
+}
+
+sendEmail();
