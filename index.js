1adeb984509b45e78e2a0c7d6c7e6ba39c3aa2df
Robbie Jaeger <jaeger.rob@gmail.com>
2019-04-11 10:34:31 -0600

    Change email markup

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function sendEmail() {
   const msg = {
     to: 'jaeger.rob@gmail.com',
     from: 'robbie@turing.io',
-    subject: 'New Lump Hammers Available',
+    subject: 'New Lump Hammer Tweet',
     html: `<h1>Lump Hammer Alert</h1>
-           <p>See if there are some new lump hammers available by Crucible Tools</p>
-           <a href="https://twitter.com/RudeMechanic" target="_blank">Chris Schwarz Twitter</a>
-           <a href="https://crucibletool.com/products/crucible-lump-hammer" target="_blank">Crucible Tool Lump Hammer</a>`
+           <h3>See if there are some new lump hammers available by Crucible Tools!</h3>
+           <p><a href="https://twitter.com/RudeMechanic" target="_blank">Chris Schwarz Twitter</a></p>
+           <p><a href="https://crucibletool.com/products/crucible-lump-hammer" target="_blank">Crucible Tool Lump Hammer</a></p>`
   };
 
   sgMail.send(msg);
